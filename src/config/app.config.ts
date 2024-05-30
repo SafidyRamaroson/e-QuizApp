5b617b3c84e4e46c8b74836b8f62af079efdc21a
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-05-30 14:38:42 +0300

    add app config

---
+++ src/config/app.config.ts
@@ -0,0 +1,10 @@
+const appConfig = {
+    corsOptions:{
+        origin:"*",
+        methods:['GET','POST','PUT','DELETE'],
+        allowedHeaders:['Content/type', 'Authorization']
+    },
+    
+}
+
+export default appConfig
