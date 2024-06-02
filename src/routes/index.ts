c291c8c4e795555e04b2fa5e2d4350d4bf416f60
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-06-02 12:58:17 +0300

    create route for deleting one user account

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,10 @@
 import express,{ Router } from "express";
+import authRoutes from "./auth.route";
+import authMe from "./me.route";
 
 const router:Router = express.Router()
+
+router.use("/auth",authRoutes)
+router.use("/me",authMe)
 
 export default router
--- src/routes/me.route.ts
+++ src/routes/me.route.ts
@@ -5,5 +5,6 @@ const router:Router = express.Router()
 
 router.get("/:id",authMeController.userProfil)
 router.patch("/:id/edit",authMeController.updateUserProfil)
+router.delete("/:id/delete",authMeController.deleteUser)
 
 export default router
