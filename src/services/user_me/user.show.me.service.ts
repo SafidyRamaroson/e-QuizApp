1bcf5907b96ac48257bd6702ef9bd5bf99d5b880
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-06-02 20:30:23 +0300

    chore(usershowme):update user show me service

--- src/services/user_me/user.show.me.service.ts
+++ src/services/user_me/user.show.me.service.ts
@@ -1,21 +1,25 @@
+import { number } from "zod";
 import { httpMessage } from "../../utils/http.message";
 import getInstancePrisma from "../../utils/prisma/getInstancePrisma";
-
 
 const show_me = async(userId:number) => {
     const prisma  = getInstancePrisma()
 
-    const user  = await prisma.user.findUnique({
-        where:{
-            id:userId
+    try {
+        const user  = await prisma.user.findUnique({
+            where:{
+                id:userId
+            }
+        })
+        
+        if(!user){
+            throw new Error(httpMessage.ACCOUNT_NOT_FOUND)
         }
-    })
 
-    if(!user){
-        throw new Error(httpMessage.ACCOUNT_NOT_FOUND)
+        return user
+    } catch (error) {
+        throw new Error((error as Error)?.message)
     }
-
-    return user
 }
 
 export default show_me;
