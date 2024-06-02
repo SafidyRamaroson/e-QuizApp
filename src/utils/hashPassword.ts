1e3603fe6934d8b10281d7f7d3f76a1de425f9d9
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-06-02 11:24:56 +0300

    add an util function to get Hash password

---
+++ src/utils/hashPassword.ts
@@ -0,0 +1,11 @@
+import bcrypt from "bcrypt"
+
+const getHashedPassword = async(password:string) => {
+    const salt:string = await  bcrypt.genSalt(10)
+    
+    const hashedPassword:string  = await bcrypt.hash(password,salt)
+
+    return hashedPassword
+}
+
+export default getHashedPassword
