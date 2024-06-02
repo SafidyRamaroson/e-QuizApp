01881d5ab272e45a20717b86ec6e729498a54add
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-06-02 12:57:13 +0300

    feat(me.controller.ts):add delete user controller

--- src/controllers/me.controller.ts
+++ src/controllers/me.controller.ts
@@ -4,8 +4,7 @@ import handleError from "../utils/handleError";
 import { User } from "@prisma/client";
 import updateMeService from "../services/user_me/user.update.me";
 import getHashedPassword from "../utils/hashPassword";
-
-
+import deleteMeService from "../services/user_me/user.delete.me.service";
 
 const userProfil  = async(req:Request,res:Response) => {
     const { id } = req.params
@@ -51,7 +50,22 @@ const updateUserProfil = async(req:Request,res:Response) => {
     }
 }
 
+const deleteUser = async(req:Request,res:Response) => {
+    const { id } = req.params
+
+    try {
+        
+        await deleteMeService(Number(id))
+        res
+        .status(204)
+        .send()
+
+    } catch (error) {
+        handleError(res,error)
+    }
+}
 export default {
     userProfil,
     updateUserProfil,
+    deleteUser
 }
