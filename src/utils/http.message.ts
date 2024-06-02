35379a67e751d1958e9d5cc59b2fbca710386e8b
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-06-02 11:26:13 +0300

    create global  object http Message

---
+++ src/utils/http.message.ts
@@ -0,0 +1,7 @@
+export const httpMessage = Object.freeze({
+    ACCOUNT_NOT_FOUND:"Account user not found",
+    ACCOUNT_ALREADY_EXIST:"Account already registered",
+    ACCOUNT_CREATED:"Account created successfully",
+    WELCOME_BACK_ACCOUNT:"Welcome Back To your Account",
+    WRONG_PASSWORD:"Wrong Password",
+})
