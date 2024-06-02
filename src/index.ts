79a26ca0e90c9e63610741460814af89f37fe7a5
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-06-02 10:35:38 +0300

    chore(index.ts):update PORT Server

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from "./app"
 import dotenv from 'dotenv'
 dotenv.config()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 app.listen(PORT,()=>{
     console.log(`Server is running at : http://localhost:${PORT}`)
