7baefe44cac44e65efd3a53a91393bac3eee6eca
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-06-06 19:30:00 +0300

    feat( createQuestion.service.ts):add difficulty question to the question

--- src/services/question/createQuestion..service.ts
+++ src/services/question/createQuestion..service.ts
@@ -1,33 +1,36 @@
+import { Difficulty } from "@prisma/client";
 import getInstancePrisma from "../../utils/prisma/getInstancePrisma";
 
-const createQuestion = async(theQuestion:string,categoryId:number) => {
-    const prisma = getInstancePrisma()
+const createQuestion = async (theQuestion: string, categoryId: number,difficulty:Difficulty) => {
+    const prisma = getInstancePrisma();
+
     try {
         const matchCategory = await prisma.categoryQuizz.findUnique({
-            where:{
-                id:Number(categoryId)
+            where: {
+                id: Number(categoryId)
             }
-        })
+        });
 
-        if(!matchCategory){
-            throw new Error("Category Quizz not found")
+        if (!matchCategory) {
+            throw new Error("Category Quizz not found");
         }
-        
+
         const newQuestion = await prisma.question.create({
-            data:{
-                content:theQuestion,
-                categoryId:categoryId,
-                createdAt:new Date(),
-                updatedAt:new Date()
+            data: {
+                content: theQuestion,
+                categoryId: categoryId,
+                difficulty,
+                createdAt: new Date(),
+                updatedAt: new Date()
             }
-        })
-        
-        return newQuestion
+        });
+
+        return newQuestion;
     } catch (error) {
-        throw new Error((error as Error)?.message)
+        throw new Error((error as Error)?.message);
+    } finally {
+        await prisma.$disconnect();
     }
+};
 
-    
-}
-
-export default createQuestion
+export default createQuestion;
